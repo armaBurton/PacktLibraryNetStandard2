b441b4b34b075d8a780c86265f6384365855b854
Arma Burton <arma@armaburton.com>
2023-05-25 13:13:23 -0700

    optional parameters

--- Person.cs
+++ Person.cs
@@ -56,4 +56,13 @@ public class Person : object{
   public string SayHello(string name){
     return $"{Name} says \"Hello {name}!\"";
   }
+
+  public string OptionalParameters(string command = "Run!", double number = 0.0, bool active = true){
+    return string.Format(
+      format: "command is {0}, number is {1}, active is {2}",
+      arg0: command,
+      arg1: number, 
+      arg2: active
+    );
+  }
 }
